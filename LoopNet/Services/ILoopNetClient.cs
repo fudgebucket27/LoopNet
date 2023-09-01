722616b40f85d1ff9fad71caa0df3ac1efec98ea
Fudgey <fudgebucket27@gmail.com>
2023-09-02 06:37:14 +1000

    update xml comments

--- LoopNet/Services/ILoopNetClient.cs
+++ LoopNet/Services/ILoopNetClient.cs
@@ -9,11 +9,59 @@ namespace LoopNet.Services
 {
     public interface ILoopNetClient
     {
+        /// <summary>
+        /// Gets tickers.
+        /// </summary>
+        /// <param name="pairs">A comma seperated list of pairs, ie: LRC-ETH,LRC-WBTC</param>
+        /// <returns>The tickers</returns>
+        /// <exception cref="Exception">Thrown when there is an issue with the Loopring API, most likey one of the pairs do not exist</exception>
         Task<TickersResponse?> GetTickersAsync(string pairs);
+
+        /// <summary>
+        /// Gets the markets
+        /// </summary>
+        /// <returns>The markets</returns>
+        /// <exception cref="Exception">Thrown when there is an issue with the Loopring API</exception>
         Task<MarketsResponse?> GetMarketsAsync();
+
+        /// <summary>
+        /// Gets account information
+        /// </summary>
+        /// <param name="owner">The address to retrieve account information from</param>
+        /// <returns>The account information</returns>
+        /// <exception cref="Exception">Thrown when there is an issue with the Loopring API, most likey the address does not exist on Loopring</exception>
         Task<AccountInformationResponse?> GetAccountInformationAsync(string owner);
+
+        /// <summary>
+        /// Gets storage id
+        /// </summary>
+        /// <param name="sellTokenId">The sell token id</param>
+        /// <returns>The storage id</returns>
+        /// <exception cref="Exception">Thrown when there is an issue with the Loopring API, most likey the sellTokenId does not exist</exception>
         Task<StorageIdResponse?> GetStorageIdAsync(int sellTokenId);
+
+        /// <summary>
+        /// Gets offchain fee
+        /// </summary>
+        /// <param name="requestType">The request type</param>
+        /// <param name="feeToken">The fee token, ie LRC</param>
+        /// <param name="amount">The amount</param>
+        /// <returns>The offchain fee</returns>
+        /// <exception cref="Exception">Thrown when there is an issue with the Loopring API, most likey the fee token or request type doest not exist</exception>
         Task<OffchainFeeResponse?> GetOffchainFeeAsync(int requestType, string feeToken, string amount);
+
+
+        /// <summary>
+        /// Transfers a token to an address
+        /// </summary>
+        /// <param name="toAddress">The address to transfer the token to</param>
+        /// <param name="transferTokenSymbol">The token symbol to transfer, Only works with ETH or LRC</param>
+        /// <param name="tokenAmount">The amount of the transfer token to sell in decimals, ie 0.1m</param>
+        /// <param name="feeTokenSymbol">The token symbol to pay fees in, ie Only works with ETH or LRC</param>
+        /// <param name="memo">The memo to send,</param>
+        /// <param name="payAccountActivationFee">Whether you want to pay the toAddress account activation fee. Optional, Defaults to false</param>
+        /// <returns>The transfer token reponnse</returns>
+        /// <exception cref="Exception">Thrown when there is an issue with the Loopring API, could be due to a number of issues such as storageId, offchainFee or not having enough balance of the transfer token</exception>
         Task<TransferTokenResponse> PostTokenTransferAsync(string toAddress, string transferTokenSymbol, decimal tokenAmount, string feeTokenSymbol, string memo, bool payAccountActivationFee);
     }
 }
--- LoopNet/Services/LoopNetClient.cs
+++ LoopNet/Services/LoopNetClient.cs
@@ -15,6 +15,9 @@ using Nethereum.Util;
 
 namespace LoopNet.Services
 {
+    /// <summary>
+    /// The LoopNet Client class
+    /// </summary>
     public class LoopNetClient : ILoopNetClient, IDisposable
     {
         private protected readonly RestClient _loopNetClient;
@@ -55,12 +58,7 @@ namespace LoopNet.Services
             GC.SuppressFinalize(this);
         }
 
-        /// <summary>
-        /// Gets tickers.
-        /// </summary>
-        /// <param name="pairs">A comma seperated list of pairs, ie: LRC-ETH,LRC-WBTC</param>
-        /// <returns>The tickers</returns>
-        /// <exception cref="Exception">Thrown when there is an issue with the Loopring API, most likey one of the pairs do not exist</exception>
+        /// <inheritdoc />
         public async Task<TickersResponse?> GetTickersAsync(string pairs)
         {
             var request = new RestRequest("api/v3/ticker");
@@ -76,11 +74,7 @@ namespace LoopNet.Services
             }
         }
 
-        /// <summary>
-        /// Gets the markets
-        /// </summary>
-        /// <returns>The markets</returns>
-        /// <exception cref="Exception">Thrown when there is an issue with the Loopring API</exception>
+        /// <inheritdoc/>
         public async Task<MarketsResponse?> GetMarketsAsync()
         {
             var request = new RestRequest("api/v3/exchange/markets");
@@ -95,12 +89,7 @@ namespace LoopNet.Services
             }
         }
 
-        /// <summary>
-        /// Gets account information
-        /// </summary>
-        /// <param name="owner">The address to retrieve account information from</param>
-        /// <returns>The account information</returns>
-        /// <exception cref="Exception">Thrown when there is an issue with the Loopring API, most likey the address does not exist on Loopring</exception>
+        /// <inheritdoc/>
         public async Task<AccountInformationResponse?> GetAccountInformationAsync(string owner)
         {
             var request = new RestRequest("api/v3/account");
@@ -159,12 +148,7 @@ namespace LoopNet.Services
             }
         }
 
-        /// <summary>
-        /// Gets storage id
-        /// </summary>
-        /// <param name="sellTokenId">The sell token id</param>
-        /// <returns>The storage id</returns>
-        /// <exception cref="Exception">Thrown when there is an issue with the Loopring API, most likey the sellTokenId does not exist</exception>
+        /// <inheritdoc/>
         public async Task<StorageIdResponse?> GetStorageIdAsync(int sellTokenId)
         {
             var request = new RestRequest("api/v3/storageId");
@@ -182,14 +166,7 @@ namespace LoopNet.Services
             }
         }
 
-        /// <summary>
-        /// Gets offchain fee
-        /// </summary>
-        /// <param name="requestType">The request type</param>
-        /// <param name="feeToken">The fee token, ie LRC</param>
-        /// <param name="amount">The amount</param>
-        /// <returns>The offchain fee</returns>
-        /// <exception cref="Exception">Thrown when there is an issue with the Loopring API, most likey the fee token or request type doest not exist</exception>
+        /// <inheritdoc/>
         public async Task<OffchainFeeResponse?> GetOffchainFeeAsync(int requestType, string feeToken, string amount)
         {
             var request = new RestRequest("api/v3/user/offchainFee");
@@ -209,17 +186,7 @@ namespace LoopNet.Services
             }
         }
 
-        /// <summary>
-        /// Transfers a token to an address
-        /// </summary>
-        /// <param name="toAddress">The address to transfer the token to</param>
-        /// <param name="transferTokenSymbol">The token symbol to transfer, ie ETH</param>
-        /// <param name="tokenAmount">The amount of the transfer token to sell in decimal, ie 0.1m</param>
-        /// <param name="feeTokenSymbol">The token symbol to pay fees in, ie LRC</param>
-        /// <param name="memo">The memo to send,</param>
-        /// <param name="payAccountActivationFee">Whether you want to pay the toAddress account activation fee. Optional, Defaults to false</param>
-        /// <returns>The transfer token reponnse</returns>
-        /// <exception cref="Exception">Thrown when there is an issue with the Loopring API, could be due to a number of issues such as storageId, offchainFee or not having enough balance of the transfer token</exception>
+        /// <inheritdoc/>
         public async Task<TransferTokenResponse> PostTokenTransferAsync(string toAddress, string transferTokenSymbol, decimal tokenAmount, string feeTokenSymbol,  string memo, bool payAccountActivationFee = false)
         {
             int feeTokenId = 0; //Default to 0 for ETH, 1 is LRC
